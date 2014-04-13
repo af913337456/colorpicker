93c0733318e0a881286107693566e6bacb732e39
stylesecond <490185202@qq.com>
2014-04-13 10:57:02 +0800

    add show color
    add show color

--- src/com/style/colorpicker/MainActivity.java
+++ src/com/style/colorpicker/MainActivity.java
@@ -1,6 +1,9 @@
 package com.style.colorpicker;
 
+import com.style.colorpicker.ColorPickerView.onColorChangedListener;
+
 import android.os.Bundle;
+import android.widget.Toast;
 import android.app.Activity; 
 
 public class MainActivity extends Activity {
@@ -9,6 +12,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ColorPickerView colorPickerView = (ColorPickerView) findViewById(R.id.cpv);
+        colorPickerView.setColorChangedListener(new onColorChangedListener() {
+			
+			@Override
+			public void colorChanged(int red, int blue, int green) {
+				Toast.makeText(MainActivity.this, "red="+red+"  blue="+blue+"   green="+green,
+						Toast.LENGTH_SHORT).show();
+				
+			}
+		});
     }
 
  
